4d892bd3d9f2d14ed6f9c2b17287c90d2fb7164b
Cnels1986 <CNels1986@gmail.com>
2018-07-13 12:48:10 -0400

    begun reading the book, Python Crash Course (https://nostarch.com/pythoncrashcourse). Completed Chp 2

---
+++ Python/helloworld.py
@@ -0,0 +1,12 @@
+# message = "hello world!!!"
+# print(message.upper())
+# print(message.lower())
+
+# first_name = "Chris"
+# last_name = "nelson"
+# print(first_name + " " + last_name.title())
+
+# print("This\nwill\nbe\nmultiple\nlines")
+
+name = "Chris Nelson"
+print("Hello, " + name + " this is a python program.")
